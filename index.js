4c5ccd5c80a12ca0ef8bf4a2ff552e5d5288f570
pgrmrmortuja <moutasimmortuja@gmail.com>
2025-08-07 17:37:18 +0600

    create and use get route

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const express = require("express");
 // Creating an express application
 const app = express();
 
+// Route handler for the root path ("/")
+// When someone visits http://localhost:5000/
+app.get("/", (req, res) => {
+    // Sending a response text to the browser
+    res.send("The Server is Working");
+});
+
+
+// Route handler for "/users" path
+// When someone visits http://localhost:5000/users
+app.get("/users", (req, res) => {
+    // Sending an empty array as the response
+    res.send([]);
+});
+
+
+
+
 // Starting the server on port 5000
 app.listen(5000, () => {
     console.log("The Server is Running on 5000 Port");
